38d674766439b46f07ebad58ac090f1e3559efac
Katrina Salceda <katrinasalceda@yahoo.co.nz>
2016-05-27 12:54:30 +1200

    hello

--- app/src/main/java/rktechltd/aucklandfishing/MapsActivity.java
+++ app/src/main/java/rktechltd/aucklandfishing/MapsActivity.java
@@ -38,9 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Auckland and move the camera
+        LatLng auckland = new LatLng(-36.848461, 174.763336);
+        mMap.addMarker(new MarkerOptions().position(auckland).title("You are in Auckland"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(auckland));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 }
