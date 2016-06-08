7a102e8e7acd8122d0589041de84c8527499f18c
Katrina Salceda <katrinasalceda@yahoo.co.nz>
2016-06-08 22:18:30 +1200

    hello

---
+++ app/src/main/java/rktechltd/aucklandfishing/adapters/FCatchAdapter.java
@@ -0,0 +1,93 @@
+package rktechltd.aucklandfishing.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rktechltd.aucklandfishing.R;
+import rktechltd.aucklandfishing.models.FishCatch;
+import rktechltd.aucklandfishing.utilities.ImageHelper;
+
+/**
+ * Created by KatSalceda on 8/06/16.
+ */
+public class FCatchAdapter extends ArrayAdapter<FishCatch> {
+    private List list;
+    public FCatchAdapter (Context context) {
+
+        super(context, R.layout.fcatchlist);
+        list = new ArrayList<FishCatch>();
+        Log.d("Adapter", "constructor");
+    }
+
+    @Override
+    public void add(FishCatch object) {
+        list.add(object);
+        Log.d("Adapter","added");
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public FishCatch getItem(int position) {
+        Log.d("Adapter","getting an item");
+        return (FishCatch)list.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.d("Adapter","getting view");
+        View row = convertView;
+        // Check if an existing view is being reused, otherwise inflate the view
+        FishCatchHolder fishcatchHolder; // view lookup cache stored in tag
+        if (convertView == null) {
+            // LayoutInflater inflater = (LayoutInflater)this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            row = inflater.inflate(R.layout.fishlist,parent, false);
+            fishcatchHolder = new FishCatchHolder();
+            fishcatchHolder.txtName = (TextView) row.findViewById(R.id.tvCatchName);
+            fishcatchHolder.txtWeight = (TextView) row.findViewById(R.id.tvCatchWeight);
+            fishcatchHolder.txtLength= (TextView) row.findViewById(R.id.tvCatchLength);
+            fishcatchHolder.txtRemarks = (TextView) row.findViewById(R.id.tvCatchRemarks);
+            fishcatchHolder.img = (ImageView)row.findViewById(R.id.imgCatch);
+            row.setTag(fishcatchHolder);
+        } else {
+            fishcatchHolder = (FishCatchHolder) row.getTag();
+        }
+
+    // Get the data item for this position
+    FishCatch fc = getItem(position);
+    Log.d("Adapter", "" + fc.getFishCatchId());
+    fishcatchHolder.txtName.setText("FISH CATCH NAME: " + fc.getName());
+    fishcatchHolder.txtWeight .setText("WEIGHT: " + fc.getWeight());
+    fishcatchHolder.txtLength.setText("LENGTH: " + fc.getLength());
+    fishcatchHolder.txtRemarks.setText("REMARKS: " + fc.getRemark());
+    fishcatchHolder.img.setImageBitmap(ImageHelper.convertImage(fc.getPicture()));
+
+    Log.d("Adapter", "returning view");
+    // Return the completed view to render on screen
+    return row;
+
+}
+
+    // View lookup cache
+    static class FishCatchHolder
+    {
+        TextView txtName = null;
+        TextView txtWeight = null;
+        TextView txtLength = null;
+        TextView txtRemarks = null;
+        ImageView img = null;
+    }
+}
---
+++ app/src/main/java/rktechltd/aucklandfishing/db/backgroundTasks/FCatchBackgroundTask.java
@@ -0,0 +1,99 @@
+package rktechltd.aucklandfishing.db.backgroundTasks;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import rktechltd.aucklandfishing.R;
+import rktechltd.aucklandfishing.adapters.FCatchAdapter;
+import rktechltd.aucklandfishing.db.daos.implementations.FishCatchDAO;
+import rktechltd.aucklandfishing.models.FishCatch;
+
+/**
+ * Created by KatSalceda on 8/06/16.
+ */
+public class FCatchBackgroundTask extends AsyncTask<String, FishCatch,String> {
+    Context ctc;
+    FCatchAdapter fcatchAdapter;
+    Activity activity;
+    ListView lv;
+
+    public FCatchBackgroundTask(Context ctc) {
+        Log.d("BG TASK", "inside constructor");
+        this.ctc = ctc;
+        fcatchAdapter = new FCatchAdapter(ctc);
+        activity = (Activity) ctc;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Log.d("BG", "setting adapter");
+        if (result.equals("Reading DB")) {
+            lv.setAdapter(fcatchAdapter);
+        } else {
+            Toast.makeText(ctc, result, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onProgressUpdate(FishCatch... values) {
+        Log.d("BG", "On progress update");
+        fcatchAdapter.add(values[0]);
+        Log.d("BG", "populating");
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String method = params[0];
+        FishCatchDAO fcdao;
+
+        if (method.equals("C")) {
+            return "C";
+        } else if (method.equals("R")) {
+            lv = (ListView) activity.findViewById(R.id.listViewCatch);
+            fcdao = new FishCatchDAO(ctc);
+            fcatchAdapter = new FCatchAdapter(ctc);
+            Log.d("BG", fcatchAdapter.toString());
+            int id;
+            String catchName;
+            String remark;
+            Double weight, length;
+
+            Cursor cursor = fcdao.getAllFishCatch();
+            if (cursor == null) {
+                cursor.moveToFirst();
+                Log.d("BG", "" + cursor.getCount());
+                while (cursor.moveToNext()) {
+                    id = cursor.getInt(0);
+                    catchName = cursor.getString(1);
+                    weight = cursor.getDouble(2);
+                    length = cursor.getDouble(3);
+                    remark = cursor.getString(6);
+
+                    FishCatch exp = new FishCatch(id, fx, length, weight, picture, name, remark);
+                    publishProgress(exp);
+                    fcatchAdapter.add(exp);
+                    Log.d("BG", "" + fcatchAdapter.getCount());
+                }
+            }
+            return "Reading DB";
+        } else {
+            return ("No fishing experience to load");
+        }
+    }
+}
